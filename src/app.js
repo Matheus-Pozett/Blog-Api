9e9bc1947bf0126438e7b211491c612cd75e26e0
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-24 10:28:18 -0300

    feat: adiciona lib async-errors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+require('express-async-errors');
 const express = require('express');
-
+const { UserRouter } = require('./routes');
+const errorMiddleware = require('./middleware/error.middleware');
 // ...
 
 const app = express();
@@ -10,9 +12,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use(express.json());
+app.use(UserRouter);
+app.use(errorMiddleware);
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
