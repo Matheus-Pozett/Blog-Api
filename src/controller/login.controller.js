84e8e2018af19abe7417b2d7c21ca2752b19bbdf
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-24 10:23:41 -0300

    feat: cria controller para login

---
+++ src/controller/login.controller.js
@@ -0,0 +1,10 @@
+const { LoginService } = require('../service/index');
+
+const login = async (req, res) => {
+  const loginData = req.body;
+  const token = await LoginService.login(loginData);
+
+  return res.status(200).json(token);
+};
+
+module.exports = { login };
