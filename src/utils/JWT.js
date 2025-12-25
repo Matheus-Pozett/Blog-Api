d6f8d5f5b47abecbc8171db3f95f20f13aac5fde
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-25 11:59:34 -0300

    feat: cria função para validar token JWT

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken');
+const AuthError = require('../Errors/AuthError');
 
 const TOKEN_SECRET = process.env.JWT_SECRET || 'secretJWT';
 
 const jwtConfig = {
-  expiresIn: '7d',
+  expiresIn: '1d',
   algorithm: 'HS256',
 };
 
 const generateToken = (payload) => jwt.sign(payload, TOKEN_SECRET, jwtConfig);
 
-module.exports = { generateToken };
+const verifyToken = (token) => jwt.verify(token, TOKEN_SECRET, (err, data) => {
+  if (err) throw new AuthError('Expired or invalid token', 401);
+  return data;
+});
+
+module.exports = { generateToken, verifyToken };
