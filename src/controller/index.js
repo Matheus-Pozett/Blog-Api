090a88f777e6856f8acb3695bfaadd2f077d5a00
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-26 10:58:32 -0300

    feat: controller para /post

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,5 +1,10 @@
 const LoginController = require('./login.controller');
 const UserController = require('./user.controller');
 const CategoriesController = require('./categories.controller');
+const PostController = require('./post.controller');
 
-module.exports = { LoginController, UserController, CategoriesController };
+module.exports = { LoginController, 
+  UserController,
+  CategoriesController,
+  PostController,
+};
