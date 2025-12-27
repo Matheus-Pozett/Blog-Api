9077bc0a8de2fac2ebae2b25c71ca75d711e39bc
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-27 20:49:41 -0300

    test: implementa testes para model Category

---
+++ tests/unit/models/categoryModel.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const { expect } = chai;
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+const CategoryModel = require('../../../src/models/Category');
+
+chai.use(sinonChai);
+
+describe('Testes de Unidade: Model de Category', function () {
+  const Category = CategoryModel(sequelize, dataTypes);
+  const category = new Category();
+  console.log(Category)
+  console.log(category)
+  describe('Nome da model', function () {
+    checkModelName(Category)('Category')
+  })
+
+  describe('Propriedades', function () {
+    const properties = ['name'];
+
+    properties.forEach(checkPropertyExists(category))
+  })
+})
