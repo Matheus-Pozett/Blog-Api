07bc65e51ff18ce41c3b2498879b7f93cfebb31a
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-27 20:49:24 -0300

    test: implementa testes para model User

---
+++ tests/unit/models/userModel.test.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const { expect } = chai;
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+const UserModel = require('../../../src/models/User');
+
+chai.use(sinonChai);
+
+describe('Testes de Unidade: Model de User', function () {
+  const User = UserModel(sequelize, dataTypes);
+  const user = new User();
+
+  describe('Nome da Model', () => {
+    checkModelName(User)('User');
+  });
+
+  describe('Propriedades', () => {
+    // Lista de colunas que esperamos que existam
+    const properties = ['displayName', 'email', 'password', 'image'];
+
+    // O checkPropertyExists gera um teste (it) para cada item do array
+    properties.forEach(checkPropertyExists(user));
+  });
+
+  describe('Associações', () => {
+    // Mockamos o model que será associado
+    const BlogPost = {
+      tableName: 'blog_posts', // Apenas um objeto simples para representar o model
+    };
+
+    before(() => {
+      //associate passando mock
+      User.associate({ BlogPost });
+    });
+
+    it('deve ter uma associação hasMany com BlogPost', () => {
+      expect(User.hasMany).to.have.been.calledWith(BlogPost, {
+        foreignKey: 'userId',
+        as: 'blog_posts',
+      });
+    });
+  });
+})
