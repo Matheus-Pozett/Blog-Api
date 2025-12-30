ab4fb8c4c57ed4343ac312816d733c691d641ccf
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-29 21:27:26 -0300

    test: cria testes de integração para rota /login

--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -3,77 +3,44 @@ const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const app = require('../../src/app');
-const { CategoryService } = require('../../src/service');
+const { LoginService } = require('../../src/service');
 const jwt = require('../../src/utils/JWT');
-const AuthError = require('../../src/Errors/AuthError');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 chai.use(sinonChai);
 
-describe.only('Testes de integração: /categories', function () {
-  afterEach(function () {
+describe('Testes de integração: /login', function () {
+  afterEach(function() {
     sinon.restore();
   });
-  describe("GET /categories", function () {
-    it('Deve retornar status 200 e a lista de categorias (Com Token Válido)', async function () {
-      const mockCategories = [
-        { id: 1, name: 'Inovação' },
-        { id: 2, name: 'Escola' }
-      ];
 
-      sinon.stub(jwt, 'verifyToken').returns({ id: 1, email: 'teste@teste.com' });
-      sinon.stub(CategoryService, 'getCategories').resolves(mockCategories);
+  describe('POST /login', function() {
+    it('Deve retornar status 200 e token ao efetuar login com sucesso', async function() {
+      const token = 'agw6r54QWQRg12314HKLJH';
 
-      const response = await chai.request(app).get('/categories')
-        .set('Authorization', 'Bearer ampiu1qwfp21334t2ef');
+      sinon.stub(LoginService, 'login').resolves(token);
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockCategories);
+      const response = await chai.request(app).post('/login')
+        .send({
+        email: "lewishamilton@gmail.com",
+        password: "123456"
+      });
+
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.be.deep.equal({token});
     });
 
-    it('Deve retornar status 401 se não enviar um token', async function () {
-      const response = await chai.request(app).get('/categories');
+    it('Deve retornar erro 400 se faltar campo obrigatório (Middleware)', async function() {
+      const response = await chai.request(app).post('/login')
+        .send({
+          email: "lewishamilton@gmail.com" 
+          // password faltando
+        });
 
-      expect(response.status).to.be.eq(401);
-      expect(response.body).to.be.deep.equal({ message: 'Token not found' })
+      expect(response.status).to.be.eq(400);
+      expect(response.body.message).to.exist;
     });
-
-    it('Deve retornar erro 401 caso o token seja inválido ou expirado', async function () {
-      sinon.stub(jwt, 'verifyToken').throws(new AuthError('Expired or invalid token', 401));
-
-      const response = await chai.request(app).get('/categories').set('Authorization', 'Bearer tokeninvalido');
-
-      expect(response.status).to.be.eq(401);
-      expect(response.body).to.be.deep.equal({ message: 'Expired or invalid token' })
-    });
-  });
-
-  describe('POST /categories', function () {
-    it('Deve retornar status 201 e o body com a nova categoria', async function () {
-      const mockCategory = { id: 1, name: "java" };
-
-      sinon.stub(jwt, 'verifyToken').returns({ id: 1, email: 'teste@teste.com' });
-      sinon.stub(CategoryService, 'createCategory').resolves(mockCategory);
-
-      const response = await chai.request(app).post('/categories').send({ name: 'java' })
-        .set('Authorization', 'Bearer token132165498');
-
-      expect(response.status).to.be.eq(201);
-      expect(response.body).to.deep.equal(mockCategory);
-    });
-
-    it('Deve retornar erro 400 se não enviar o campo "name"', async function () {
-      sinon.stub(jwt, 'verifyToken').returns({ id: 1, email: 'teste@teste.com' });
-
-      const response = await chai.request(app)
-        .post('/categories')
-        .send({})
-        .set('Authorization', 'Bearer token_valido');
-
-      expect(response.status).to.be.equal(400);
-      expect(response.body.message).to.equal('"name" is required');
-    });
-  });
+  })
 });
